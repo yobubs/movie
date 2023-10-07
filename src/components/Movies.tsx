4aeeaf6791dee57fe5523dc7c55015aa39429335
minard <minard>
2023-10-07 13:32:39 +0800

    fix responsiveness of movie section and make it to grid

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -31,60 +31,42 @@ const Movies = () => {
             </div>
          </div>
 
-         <h1 className="ms-8 mt-10 text-xl text-slate-200">Trending</h1>
-         <div className="movies mt-4 flex flex-wrap items-center justify-center gap-3">
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((s) => (
-               <a
-                  key={s}
-                  href="#"
-                  className="w-40 rounded-lg bg-slate-800 p-2 hover:bg-slate-700 sm:w-52"
-               >
-                  <div className="relative flex h-48 w-full items-center justify-center rounded-xl bg-slate-900 text-slate-400 sm:h-56">
-                     {s}
+         {['Trending', 'Latest Release'].map((v, i) => (
+            <div key={i}>
+               <h1 className="ms-8 mt-10 text-xl text-slate-200">{v}</h1>
+               <div className="movies mt-4 grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+                  {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((s) => (
+                     <a
+                        key={s}
+                        href="#"
+                        className="rounded-lg bg-slate-800 p-2 hover:bg-slate-700 "
+                     >
+                        <div className="relative flex h-56 w-full items-center justify-center rounded-xl bg-slate-900 text-slate-400 sm:h-44 md:h-36">
+                           {s}
 
-                     <div className="absolute right-2 top-1 flex items-center">
-                        <AiFillStar size="15px" className="text-yellow-400" />
-                        <p className="text-[12px]">4.5</p>
-                     </div>
-                  </div>
-                  <p className="text-lg text-slate-300">Lorem</p>
-                  <div className="flex items-center justify-between text-sm text-slate-400">
-                     <p>HD </p>
-                     <p>{s}09 min</p>
-                  </div>
-               </a>
-            ))}
-         </div>
-         <div className="mt-5 text-center">
-            <button className="rounded-xl bg-gradient-to-tr from-purple-500 to-cyan-500 px-14 py-2 text-slate-200 shadow-xl shadow-slate-950/60">
-               see more
-            </button>
-         </div>
-
-         <h1 className="ms-8 mt-10 text-xl text-slate-200">Latest Release</h1>
-         <div className="movies mt-4 flex flex-wrap items-center justify-center gap-3">
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((s) => (
-               <a
-                  key={s}
-                  href="#"
-                  className="w-40 rounded-lg bg-slate-800 p-2 hover:bg-slate-700 sm:w-52"
-               >
-                  <div className="relative flex h-48 w-full items-center justify-center rounded-xl bg-slate-900 text-slate-400 sm:h-56">
-                     {s}
-                  </div>
-                  <p className="text-lg text-slate-300">Lorem</p>
-                  <div className="flex items-center justify-between text-sm text-slate-400">
-                     <p>HD </p>
-                     <p>{s}09 min</p>
-                  </div>
-               </a>
-            ))}
-         </div>
-         <div className="mt-5 text-center">
-            <button className="rounded-xl bg-gradient-to-tr from-purple-500 to-cyan-500 px-14 py-2 text-slate-200 shadow-xl shadow-slate-950/60">
-               see more
-            </button>
-         </div>
+                           <div className="absolute right-2 top-1 flex items-center">
+                              <AiFillStar
+                                 size="15px"
+                                 className="text-yellow-400"
+                              />
+                              <p className="text-[12px]">4.5</p>
+                           </div>
+                        </div>
+                        <p className="text-lg text-slate-300">Lorem</p>
+                        <div className="flex items-center justify-between text-sm text-slate-400">
+                           <p>HD </p>
+                           <p>{s}09 min</p>
+                        </div>
+                     </a>
+                  ))}
+               </div>
+               <div className="mt-5 text-center">
+                  <button className="rounded-xl bg-gradient-to-tr from-purple-500 to-cyan-500 px-14 py-2 text-slate-200 shadow-xl shadow-slate-950/60">
+                     see more
+                  </button>
+               </div>
+            </div>
+         ))}
       </main>
    )
 }
